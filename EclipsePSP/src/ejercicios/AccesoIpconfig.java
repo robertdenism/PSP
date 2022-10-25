2c1458311b43cd270c40fc0e175bad3f362a79b1
robertdenism <robert1994denis@gmail.com>
2022-10-25 19:59:11 +0200

    ejip

---
+++ EclipsePSP/src/ejercicios/AccesoIpconfig.java
@@ -0,0 +1,34 @@
+package ejercicios;
+
+import java.io.*;
+
+	public class AccesoIpconfig {
+		public static void main(String[] args) throws IOException {
+			
+		
+			Process p = new ProcessBuilder("CMD","/C", "ipconfig").start();
+			try {
+
+				InputStream is = p.getInputStream();
+
+				// mostramos en pantalla caracter a caracter
+				 int c;
+				 while ((c = is.read()) != -1)
+					System.out.print((char) c);
+				 is.close();
+
+			
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			// COMPROBACION DE ERROR - 0 bien - 1 mal
+			int exitVal;
+			try {
+				exitVal = p.waitFor();
+				System.out.println("Valor de Salida: " + exitVal);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+}
